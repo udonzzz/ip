0c557f9ab333b0211e7d9bdd52af7666c87bd039
adon <127666594+udonzzz@users.noreply.github.com>
2025-08-17 21:28:51 +0800

    Modify TaskList.java
    Modify TaskList Class to hold Task Objects instead of Strings.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 
 public class TaskList {
-    private ArrayList<String> list;
+    private ArrayList<Task> list;
 
     public TaskList() {
-        this.list = new ArrayList<String>();
+        this.list = new ArrayList<Task>();
     }
 
-    void addTask(String task) {
-        list.add(task);
+    void addTask(String description) {
+        list.add(new Task(description));
     }
 
     @Override
     public String toString() {
         String output = "";
         for (int i = 0; i < list.size(); i++) {
-            output += Integer.toString(i + 1) + ". " + list.get(i) + "\n";
+            output += Integer.toString(i + 1) + ". " + list.get(i).toString() + "\n";
         }
         return output;
     }
