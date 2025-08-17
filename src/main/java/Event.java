902e7497808283f113061a5da3cf9892feabe723
adon <127666594+udonzzz@users.noreply.github.com>
2025-08-18 00:50:49 +0800

    Add Event.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+public class Event extends Task {
+    protected String type;
+    protected String start;
+    protected String end;
+
+    public Event(String description, String start, String end) {
+        super(description);
+        this.status = "[E]";
+        this.start = start;
+        this.end = end;
+    }
+    
+    @Override
+    public String toString() {
+        return type + status + " " + description + "(from: " + start + " to: "  + end + ")";
+    }
+}
