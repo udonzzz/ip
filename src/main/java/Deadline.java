fd00c2477f88cf4d33728be8846fa1f44575e912
adon <127666594+udonzzz@users.noreply.github.com>
2025-08-18 00:45:38 +0800

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,15 @@
+public class Deadline extends Task {
+    protected String type;
+    protected String deadline;
+
+    public Deadline(String description, String deadline) {
+        super(description);
+        this.type = "[D]";
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return type + status + " " + description + "(by: " + deadline + ")";
+    }
+}
