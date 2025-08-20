96c4bf889e6d906fd5b7ba25527515c4ee506ad7
adon <127666594+udonzzz@users.noreply.github.com>
2025-08-20 18:23:14 +0800

    Create PandaException.java
    PandaException will be used to manage the error message that is sent,
    based on the input provided and type of action requested.

---
+++ src/main/java/PandaException.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PandaException extends Exception {
+    private static ArrayList<String> actionList = new ArrayList<String>(List.of("add", "list", "mark",
+            "unmark", "todo", "deadline", "event"));
+
+    public PandaException(String action, String description) {
+        super(createMessage(action, description));
+    }
+
+    private static String createMessage(String action, String description) {
+        if (!actionList.contains(action)) {
+            return "OOPS!!! I'm sorry, but I don't know what that means :-(";
+        }
+        if (action.equals("mark") || action.equals("unmark")) {
+            return "OOPS!!! " + action + " needs to be provided an integer as such: \n"
+                    + action + " (insert list no. of task to mark)";
+        }
+        if (description.equals("")) {
+            return "OOPS!!! The description of a " + action + " cannot be empty.";
+        }
+        if (action.equals("deadline")) {
+            return "OOPS!!! Please provide the deadline as such: \n"
+                    + "deadline (insert description) /by (insert deadline)";
+        }
+        if (action.equals("event")) {
+                return "OOPS!!! Please provide the duration as such: \n"
+                        +  "event /from (insert start date) /to (insert end date)";
+        }
+        return "OOPS!!! Unknown error occurred";
+    }
+}
