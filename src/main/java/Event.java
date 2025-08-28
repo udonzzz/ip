a3b3902d7f593711bb7b9a1985e41357bae9a8dd
adon <127666594+udonzzz@users.noreply.github.com>
2025-08-29 03:10:20 +0800

    Modify dates in tasks to be LocalDate type
    Deadline and Event tasks will now handle dates as LocalDate instead
    of a String, and format them to the specified format.
    
    Panda.java will also catch DateTimeParseException if date format
    provided is wrong.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,29 +1,36 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     protected String type;
-    protected String start;
-    protected String end;
+    protected LocalDate start;
+    protected LocalDate end;
 
     public Event(String description, String start, String end) {
         super(description);
         this.type = "[E]";
-        this.start = start;
-        this.end = end;
+        this.start = LocalDate.parse(start);
+        this.end = LocalDate.parse(end);
     }
 
     public Event(String status, String description, String start, String end) {
         super(status, description);
         this.type = "[E]";
-        this.start = start;
-        this.end = end;
+        this.start = LocalDate.parse(start, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+        this.end = LocalDate.parse(end, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+    }
+
+    private String formatDate(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
     }
 
     public String writeToFile() {
         return type + "\u2022" + super.status + "\u2022" + super.description
-                + "\u2022" + start +  "\u2022" + end + "\n";
+                + "\u2022" + formatDate(start) +  "\u2022" + formatDate(end) + "\n";
     }
 
     @Override
     public String toString() {
-        return type + super.toString() + " (from: " + start + " to: "  + end + ")";
+        return type + super.toString() + " (from: " + formatDate(start) + " to: "  + formatDate(end) + ")";
     }
 }
