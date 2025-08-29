e91680d1e95e49fcf2f56e311a7bada05a7c7a09
adon <127666594+udonzzz@users.noreply.github.com>
2025-08-29 23:47:48 +0800

    Modify Panda.java
    With the new classes, the bulk of code in Panda.java previously has
    been abstracted out.
    
    Panda class will now have ui, fileManager, parser and tasks as its
    variables, which will perform their respective roles.

--- src/main/java/Panda.java
+++ src/main/java/Panda.java
@@ -1,81 +1,33 @@
-import java.io.IOException;
-import java.io.FileWriter;
-import java.time.format.DateTimeParseException;
-
 public class Panda {
+    private PandaUi ui;
+    private FileManager fileManager;
+    private InputParser parser;
+    private TaskList tasks;
 
-    private static boolean arraySizeWrong(String[] array, int expectedSize) {
-        return  array.length != expectedSize;
+    public Panda(String filePath) {
+        ui = new PandaUi();
+        fileManager = new FileManager(filePath);
+        parser = new InputParser();
+        tasks = new TaskList();
+        fileManager.loadTasks(tasks, ui);
     }
 
-    public static void main(String[] args) throws IOException{
-        String pathName = "panda.txt";
-        PandaUi ui = new PandaUi();
-        FileManager fileManager = new FileManager(pathName);
-        TaskList tasks = new TaskList();
-        fileManager.loadTasks(tasks, ui);
+    public void run() {
         ui.greet();
-        while (true) {
+        boolean isExit = false;
+        while (!isExit) {
             try {
-                String input = ui.userInput();
-                String[] strArray = input.split(" ", 2);
-                String action = strArray[0];
-                if (action.equals("bye")) {
-                    ui.bye();
-                    break;
-                }
-                if (action.equals("list")) {
-                    ui.reply(tasks);
-                    continue;
-                }
-                if (arraySizeWrong(strArray, 2)) {
-                    throw new PandaException(action, "");
-                }
-                if (action.equals("mark")) {
-                    Task task = tasks.changeStatus(strArray[1], action);
-                    ui.taskMarked(task);
-                    continue;
-                }
-                if (action.equals("unmark")) {
-                    Task task = tasks.changeStatus(strArray[1], action);
-                    ui.taskUnmarked(task);
-                    continue;
-                }
-                if (action.equals("delete")) {
-                    Task task = tasks.deleteTask(strArray[1]);
-                    ui.taskDeleted(task, tasks);
-                    continue;
-                }
-                if (action.equals("todo")) {
-                    Task todo = tasks.addTask(new ToDo(strArray[1]));
-                    ui.taskAdded(todo, tasks);
-                    continue;
-                }
-                if (action.equals("deadline")) {
-                    String[] infoDeadline = strArray[1].split(" /by ");
-                    if (arraySizeWrong(infoDeadline, 2)) {
-                        throw new PandaException(action, infoDeadline[0]);
-                    }
-                    Task deadline = tasks.addTask(new Deadline(infoDeadline[0], infoDeadline[1]));
-                    ui.taskAdded(deadline, tasks);
-                    continue;
-                }
-                if (action.equals("event")) {
-                    String[] infoEvent = strArray[1].split(" /from | /to ");
-                    if (arraySizeWrong(infoEvent, 3)) {
-                        throw new PandaException(action, infoEvent[0]);
-                    }
-                    Task event = tasks.addTask(new Event(infoEvent[0], infoEvent[1], infoEvent[2]));
-                    ui.taskAdded(event, tasks);
-                    continue;
-                }
-                throw new PandaException(action, "");
+                String input = ui.getUserInput();
+                Action a = parser.parseUserInput(input);
+                a.execute(tasks, ui, fileManager);
+                isExit = a.isExit();
             } catch (PandaException e) {
                 ui.pandaError(e);
-            } catch (DateTimeParseException e) {
-                ui.wrongDateFormat();
             }
         }
-        fileManager.saveTasks(tasks);
+    }
+
+    public static void main(String[] args) {
+        new Panda("panda.txt").run();
     }
 }
