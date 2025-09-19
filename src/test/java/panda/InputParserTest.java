12ec875f89b43f66e799f0b89e782d24aa21a617
adon <127666594+udonzzz@users.noreply.github.com>
2025-09-19 23:42:12 +0800

    Update JUnit test cases

--- src/test/java/panda/InputParserTest.java
+++ src/test/java/panda/InputParserTest.java
@@ -2,19 +2,19 @@ package panda;
 
 import panda.action.AddAction;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class InputParserTest {
 
     @Test
     public void parseUserInput_addDeadline_exceptionThrown() {
         String action = "deadline";
-        String[] taskInfo = {"test by 2025-09-01"};
-        String input = "deadline test by 2025-09-01";
-        String exceptionMessage = "OOPS!!! Please provide the deadline as such: \n"
-                + "deadline (insert description) /by (insert deadline)";
+        String[] taskInfo = {"test", "2025-09-01 10:00"};
+        String input = "deadline test by 2025-09-01 10:00";
+        String exceptionMessage = "Please provide the deadline as such: \n"
+                + "deadline (insert description) /by (insert deadline dateTime)";
         try {
             assertEquals(new AddAction(action, taskInfo), new InputParser().parseUserInput(input));
         } catch (PandaException e) {
--- src/test/java/panda/task/DeadlineTest.java
+++ src/test/java/panda/task/DeadlineTest.java
@@ -1,19 +1,19 @@
 package panda.task;
 
-import panda.PandaException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import panda.PandaException;
 
 public class DeadlineTest {
 
     @Test
     public void parseDate_invalidDate_exceptionThrown() {
-        String exceptionMessage = "Please provide a valid date in the format yyyy-MM-dd!";
+        String exceptionMessage = "Please provide a valid date in the format yyyy-MM-dd HH:mm!";
         try {
-            assertNotNull(new Deadline("test", "01-09-2025"));
+            assertNotNull(new Deadline("test", "01-09-2025 10:00"));
         } catch (PandaException e) {
             assertEquals(exceptionMessage, e.getMessage());
         }
